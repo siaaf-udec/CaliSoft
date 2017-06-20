f9e8148d508771eb7a12130f3cef8cde6633843c
Johan Suárez <johancamilosuarezcampos@gmail.com>
2017-06-19 23:32:13 -0500

    Anexo ruta y controlador del registro de evaluador

--- resources/views/layouts/admin-dash.blade.php
+++ resources/views/layouts/admin-dash.blade.php
@@ -1,7 +1,17 @@
 @extends('material.layouts.dashboard')
 
 @section('links')
-    @component('components.nav-link', ['icon' => 'fa fa-users', 'link' => '#', 'title' => 'Usuarios'])
+    @component('components.nav-dropdown', ['icon' => 'fa fa-users', 'link' => '#', 'title' => 'Usuarios'])
+
+                @component('components.nav-link', ['icon' => 'fa fa-users', 'link' => route('register-evaluador'), 'title' => 'Registrar evaluadores'])
+                @endcomponent
+
+                @component('components.nav-link', ['icon' => 'fa fa-bar-chart-o', 'link' => '#', 'title' => 'Lo que quieran poner'])
+                @endcomponent
+
+                @component('components.nav-link', ['icon' => 'fa fa-cubes', 'link' => '#', 'title' => 'Lo que quieran poner'])
+                @endcomponent
+
     @endcomponent
 
     @component('components.nav-link', ['icon' => 'fa fa-university', 'link' => '#', 'title' => 'Peticiones'])
@@ -17,8 +27,8 @@
     @endcomponent
 
     @component('components.nav-link', [
-        'icon' => 'fa fa-gears', 
-        'link' => route('semilleros'), 
+        'icon' => 'fa fa-gears',
+        'link' => route('semilleros'),
         'title' => 'Semilleros'])
     @endcomponent
 
--- routes/admin.php
+++ routes/admin.php
@@ -4,3 +4,5 @@ Route::get('/', 'AdminController@index')->name('admin');
 Route::get('/semilleros', 'AdminController@semilleros')->name('semilleros');
 Route::get('/categorias', 'AdminController@categorias')->name('categorias');
 Route::get('/usuarios', 'AdminController@usuarios')->name('usuarios');
+
+Route::get('/register-evaluador', 'RegisterEvaluadorController@index')->name('register-evaluador');
