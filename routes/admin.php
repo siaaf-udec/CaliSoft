7d440a4ffe2e79cca2ba92bf6f8da67dd4a48a21
EfrainAndres <efrainvergara.udec@gmail.com>
2017-06-20 16:24:09 -0500

    Adelanto "Tipos De Documentos"
    Falta Crear
    Lista OK

--- routes/admin.php
+++ routes/admin.php
@@ -4,4 +4,4 @@ Route::get('/', 'AdminController@index')->name('admin');
 Route::get('/semilleros', 'AdminController@semilleros')->name('semilleros');
 Route::get('/categorias', 'AdminController@categorias')->name('categorias');
 Route::get('/usuarios', 'AdminController@usuarios')->name('usuarios');
-
+Route::get('/tdocumento', 'TiposDocumentoController@index')->name('tdocumento');
