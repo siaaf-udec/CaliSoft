8d1a2cd758ccebfd6f500e0db5cbe2f68d41d0a7
jhfredy <jhfredy84@hotmail.com>
2017-06-20 12:19:05 -0500

    creacion de botones categorias

--- resources/views/admin/categorias.blade.php
+++ resources/views/admin/categorias.blade.php
@@ -1,7 +1,14 @@
 @extends('layouts.admin-dash')
 @section('content')
-    <div style="" >
+    <div class="form-group row add" >
+    <div class="col-md-12">
             <h3 class="text-center">CONFIGURACIÓN DE PORCENTAJES</h3>
+    </div>
+    <div class="col-md-12">
+    <button type="button" data-toggle="modal" data-target="#crear-categoria" class="btn btn-primary">
+    Crear Nueva Categoría
+    </button>
+    </div>
     </div>
        
     <div id="app">
@@ -12,10 +19,11 @@
             <th>Plataforma</th>
             <th>Modelado</th>
             <th>Despliegue</th>
-            <th>Entidad/relacion</th>
+            <th>Entidad/relación</th>
             <th>Clases</th>
             <th>Actividades</th>
             <th>Uso</th>
+            <th>Operaciones</th>
         </thead>
         <tbody>
             <tr v-for="categoria in categorias">
@@ -27,6 +35,16 @@
                 <td v-text="categoria.clases"></td>
                 <td v-text="categoria.actividades"></td>
                 <td v-text="categoria.uso"></td>
+                <td>
+                <button class="editar-modal btn btn-warning" @click.prevent="editarCategoria(categoria)">
+                <span class="glyphicon glyphicon-edit"></span>Editar
+                </button>
+                </td>
+                <td>
+                <button class="editar-modal btn btn-danger" @click.prevent="eliminarCategoria(categoria)">
+                <span class="glyphicon glyphicon-trash"></span>Eliminar
+                </button>
+                </td>
             </tr>
         </tbody>
     </table>
