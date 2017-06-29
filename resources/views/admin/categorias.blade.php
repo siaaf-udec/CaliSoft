93d33f6a8f34c76653425c7349600e1394c77314
jhfredy <jhfredy84@hotmail.com>
2017-06-29 13:54:22 -0500

    holi

--- resources/views/admin/categorias.blade.php
+++ resources/views/admin/categorias.blade.php
@@ -3,7 +3,13 @@
 @section('content')
 <div class="col-md-12">
     @component('components.portlet', ['icon' => 'fa fa-pie-chart', 'title' => 'Categorias'])
+        @slot('actions',[
+            'link_question' => [
+            'link' => '#modal-ayuda',
+            'icon' => 'icon-question',
 
+                    ],
+        ])
 
         <div id="app">
 
@@ -295,8 +301,28 @@
             </modal>
             <!-- Fin Modal Eliminar -->
         </div>
+        <div id="modal-ayuda" class="modal fade" role="dialog">
+  <div class="modal-dialog">
+
+    <!-- Modal content-->
+    <div class="modal-content">
+      <div class="modal-header">
+        <button type="button" class="close" data-dismiss="modal">&times;</button>
+        <h4 class="modal-title">Modal Header</h4>
+      </div>
+      <div class="modal-body">
+        <p>Some text in the modal.</p>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+      </div>
+    </div>
+
+  </div>
+</div>
     @endcomponent
 </div>
+
 @endsection
 
 @push('styles')
