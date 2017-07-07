a7b17dfed4132ac6e0aac9cfc078e539c1c6973c
Stevenson Marquez <stevensonxmarquez@gmail.com>
2017-07-07 17:51:22 -0500

    Corregido en parametro de ruta de componentes

--- routes/admin.php
+++ routes/admin.php
@@ -6,4 +6,4 @@ Route::get('/categorias', 'AdminController@categorias')->name('categorias');
 Route::get('/usuarios', 'AdminController@usuarios')->name('usuarios');
 Route::get('/tdocumentos', 'AdminController@tipoDocumento')->name('tdocumentos');
 Route::get('/proyecto', 'AdminController@proyectos')->name('proyecto.admin');
-Route::get('/tdocumentos/{documento}/componentes', 'AdminController@componentes')->name('componentes');
+Route::get('/tdocumentos/{tdocumento}/componentes', 'AdminController@componentes')->name('componentes');
