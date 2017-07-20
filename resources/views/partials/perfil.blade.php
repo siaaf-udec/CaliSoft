424ebd55f389ed4a4c7643ca749a187434f08da2
jhfredy <jhfredy84@hotmail.com>
2017-07-19 19:00:33 -0500

    plantilla oficial mi perfil

--- resources/views/partials/perfil.blade.php
+++ resources/views/partials/perfil.blade.php
@@ -1,6 +1,6 @@
 <div class="row">
 
-    <div class="col-md-6">
+    <div class="col-md-4">
         <div class="portlet light profile-sidebar-portlet ">
             <!-- SIDEBAR USERPIC -->
             <div class="profile-userpic">
@@ -37,10 +37,44 @@
             </div>
             <!-- END MENU -->
         </div>
+        <div class="portlet light ">
+            <!-- STAT -->
+            <div class="row list-separated profile-stat">
+                <div class="col-md-4 col-sm-4 col-xs-6">
+                    <div class="uppercase profile-stat-title"> 37 </div>
+                    <div class="uppercase profile-stat-text"> Projects </div>
+                </div>
+                <div class="col-md-4 col-sm-4 col-xs-6">
+                    <div class="uppercase profile-stat-title"> 51 </div>
+                    <div class="uppercase profile-stat-text"> Tasks </div>
+                </div>
+                <div class="col-md-4 col-sm-4 col-xs-6">
+                    <div class="uppercase profile-stat-title"> 61 </div>
+                    <div class="uppercase profile-stat-text"> Uploads </div>
+                </div>
+            </div>
+            <!-- END STAT -->
+            <div>
+                <h4 class="profile-desc-title">About Marcus Doe</h4>
+                <span class="profile-desc-text"> Lorem ipsum dolor sit amet diam nonummy nibh dolore. </span>
+                <div class="margin-top-20 profile-desc-link">
+                    <i class="fa fa-globe"></i>
+                    <a href="http://www.keenthemes.com">www.keenthemes.com</a>
+                </div>
+                <div class="margin-top-20 profile-desc-link">
+                    <i class="fa fa-twitter"></i>
+                    <a href="http://www.twitter.com/keenthemes/">@keenthemes</a>
+                </div>
+                <div class="margin-top-20 profile-desc-link">
+                    <i class="fa fa-facebook"></i>
+                    <a href="http://www.facebook.com/keenthemes/">keenthemes</a>
+                </div>
+            </div>
+        </div>
     </div>
 
 
-    <div class="col-md-6">
+    <div class="col-md-8">
         <div class="profile-content">
 
             <div class="portlet-title tabbable-line">
@@ -222,26 +256,26 @@
         </div>
     </div>
     @push('styles')
-      <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
-           <link href="../assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
-           <link href="../assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
-           <link href="../assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-           <link href="../assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
-           <!-- END GLOBAL MANDATORY STYLES -->
-           <!-- BEGIN PAGE LEVEL PLUGINS -->
-           <link href="../assets/global/plugins/bootstrap-fileinput/bootstrap-fileinput.css" rel="stylesheet" type="text/css" />
-           <!-- END PAGE LEVEL PLUGINS -->
-           <!-- BEGIN THEME GLOBAL STYLES -->
-           <link href="../assets/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css" />
-           <link href="../assets/global/css/plugins.min.css" rel="stylesheet" type="text/css" />
-           <!-- END THEME GLOBAL STYLES -->
-           <!-- BEGIN PAGE LEVEL STYLES -->
-           <link href="../assets/pages/css/profile.min.css" rel="stylesheet" type="text/css" />
-           <!-- END PAGE LEVEL STYLES -->
-           <!-- BEGIN THEME LAYOUT STYLES -->
-           <link href="../assets/layouts/layout2/css/layout.min.css" rel="stylesheet" type="text/css" />
-           <link href="../assets/layouts/layout2/css/themes/blue.min.css" rel="stylesheet" type="text/css" id="style_color" />
-           <link href="../assets/layouts/layout2/css/custom.min.css" rel="stylesheet" type="text/css" />
-           <!-- END THEME LAYOUT STYLES -->
-           <link rel="shortcut icon" href="favicon.ico" /> </head>
+    <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
+    <link href="../assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+    <link href="../assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
+    <link href="../assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+    <link href="../assets/global/plugins/bootstrap-switch/css/bootstrap-switch.min.css" rel="stylesheet" type="text/css" />
+    <!-- END GLOBAL MANDATORY STYLES -->
+    <!-- BEGIN PAGE LEVEL PLUGINS -->
+    <link href="../assets/global/plugins/bootstrap-fileinput/bootstrap-fileinput.css" rel="stylesheet" type="text/css" />
+    <!-- END PAGE LEVEL PLUGINS -->
+    <!-- BEGIN THEME GLOBAL STYLES -->
+    <link href="../assets/global/css/components.min.css" rel="stylesheet" id="style_components" type="text/css" />
+    <link href="../assets/global/css/plugins.min.css" rel="stylesheet" type="text/css" />
+    <!-- END THEME GLOBAL STYLES -->
+    <!-- BEGIN PAGE LEVEL STYLES -->
+    <link href="../assets/pages/css/profile.min.css" rel="stylesheet" type="text/css" />
+    <!-- END PAGE LEVEL STYLES -->
+    <!-- BEGIN THEME LAYOUT STYLES -->
+    <link href="../assets/layouts/layout2/css/layout.min.css" rel="stylesheet" type="text/css" />
+    <link href="../assets/layouts/layout2/css/themes/blue.min.css" rel="stylesheet" type="text/css" id="style_color" />
+    <link href="../assets/layouts/layout2/css/custom.min.css" rel="stylesheet" type="text/css" />
+    <!-- END THEME LAYOUT STYLES -->
+    <link rel="shortcut icon" href="favicon.ico" /> </head>
     @endpush
