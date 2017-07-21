6ec981002d5b20e161e381d1cba63fa3cd3d89b4
Johan Suárez <johancamilosuarezcampos@gmail.com>
2017-07-21 10:30:49 -0500

    Corrección para descargar

--- app/Container/Calisoft/Src/Controllers/DocumentoController.php
+++ app/Container/Calisoft/Src/Controllers/DocumentoController.php
@@ -160,7 +160,7 @@ class DocumentoController extends Controller
 
     public function download($file)
     {
-        return response()->download("../storage/uploads/documentos/" . $file);
+        return response()->download("../storage/uploads/documentos/" . $file . '.pdf');
 
         //$publicacion = Documentos::find($file);
         //$rutaarchivo = '../storage/uploads/' . $publicacion->url . '.pdf';
