b791653cfb7af4a60bf4a5740b41e054cddcd40c
EfrainAndres <efrainvergara.udec@gmail.com>
2017-08-03 11:45:36 -0500

    Datatable prueba

--- resources/views/calisoft/admin/admin-usuarios.blade.php
+++ resources/views/calisoft/admin/admin-usuarios.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.dash')
+
 @section('content')
 <div class="col-md-12">
     @component('components.portlet', ['icon' => 'fa fa-users', 'title' => 'Usuarios'])
@@ -19,14 +20,25 @@
                 </div>
 
             </div>
+
+
             <!-- Table -->
             <div class="table-responsive">
-                <table class="table table-hover table-bordered table-condensed">
+              <div class="col-md-12">
+            <br>
+            <br>
+            <br>
+            <br>
+              </div>
+                <table class="table table-hover table-bordered table-condensed" id="usuario">
                     <thead>
+                      <tr>
+
                         <th class="text-center">Nombre</th>
                         <th class="text-center">Correo</th>
                         <th class="text-center">Rol</th>
                         <th class="text-center">Operacion</th>
+                        </tr>
                     </thead>
                     <tbody>
                         <tr v-for="user in usuarios" class="text-center">
@@ -140,11 +152,19 @@
 </div>
 @endsection
 @push('styles')
+  <link href="{{ asset('assets/global/plugins/datatables/datatables.min.css') }}" rel="stylesheet" type="text/css" />
+  <link href="{{ asset('assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.css') }}" rel="stylesheet" type="text/css" />
+
   <link rel="stylesheet" href="/assets/global/plugins/bootstrap-toastr/toastr.min.css">
   <link rel="stylesheet" href="/assets/global/plugins/bootstrap-select/css/bootstrap-select.min.css">
 @endpush
 
 @push('functions')
+  <!-- Datatables Scripts -->
+  <script src="{{ asset('assets/global/scripts/datatable.js') }}" type="text/javascript"></script>
+  <script src="{{ asset('assets/global/plugins/datatables/datatables.min.js') }}" type="text/javascript"></script>
+  <script src="{{ asset('assets/global/plugins/datatables/plugins/bootstrap/datatables.bootstrap.js') }}" type="text/javascript"></script>
+
   <script src="{{ asset('assets/global/plugins/jquery-validation/js/jquery.validate.min.js') }}" type="text/javascript"></script>
   <script src="{{ asset('assets/global/plugins/jquery-validation/js/additional-methods.min.js') }}" type="text/javascript"></script>
   <script src="{{ asset('assets/global/plugins/jquery-validation/js/localization/messages_es.js') }}" type="text/javascript"></script>
@@ -154,6 +174,58 @@
   <script src="/assets/global/plugins/bootstrap-select/js/bootstrap-select.min.js"></script>
   <script src="/assets/global/plugins/bootstrap-toastr/toastr.min.js"></script>
   <script src="/js/usuarios.js"></script>
+@endpush
 
+@push('functions')
+<script>
+    jQuery(document).ready(function () {
 
+        var tabla_novedades = $('#usuario').DataTable({
+            lengthMenu: [
+                [5, 10, 25, 50, -1],
+                [5, 10, 25, 50, "Todo"]
+            ],
+            responsive: true,
+            columnDefs: [
+                { targets: 'no-sort', orderable: false }
+            ],
+            language: {
+                "bProcessing": '<i class="fa fa-spinner fa-spin fa-3x fa-fw"></i> <span class="sr-only">Procesando...</span>',
+                "sLengthMenu": "Mostrar _MENU_ registros",
+                "sZeroRecords": "No se encontraron resultados",
+                "sEmptyTable": "Ningún dato disponible en esta tabla",
+                "sInfo": "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+                "sInfoEmpty": "Mostrando registros del 0 al 0 de un total de 0 registros",
+                "sInfoFiltered": "(filtrado de un total de _MAX_ registros)",
+                "sInfoPostFix": "",
+                "sSearch": "Buscar:",
+                "sUrl": "",
+                "sInfoThousands": ",",
+                "sLoadingRecords": "Cargando...",
+                "oPaginate": {
+                    "sFirst": "Primero",
+                    "sLast": "Último",
+                    "sNext": "Siguiente",
+                    "sPrevious": "Anterior"
+                },
+                "oAria": {
+                    "sSortAscending": ": Activar para ordenar la columna de manera ascendente",
+                    "sSortDescending": ": Activar para ordenar la columna de manera descendente"
+                }
+            },
+            buttons: [
+                { extend: 'print', className: 'btn btn-circle btn-icon-only btn-default tooltips t-print', text: '<i class="fa fa-print"></i>'},
+                { extend: 'copy', className: 'btn btn-circle btn-icon-only btn-default tooltips t-copy', text: '<i class="fa fa-files-o"></i>'},
+                { extend: 'pdf', className: 'btn btn-circle btn-icon-only btn-default tooltips t-pdf', text: '<i class="fa fa-file-pdf-o"></i>'},
+                { extend: 'excel', className: 'btn btn-circle btn-icon-only btn-default tooltips t-excel', text: '<i class="fa fa-file-excel-o"></i>'},
+                { extend: 'csv', className: 'btn btn-circle btn-icon-only btn-default tooltips t-csv',  text: '<i class="fa fa-file-text-o"></i>'},
+                { extend: 'colvis', className: 'btn btn-circle btn-icon-only btn-default tooltips t-colvis', text: '<i class="fa fa-bars"></i>'}
+
+            ],
+            tabIndex: -1,
+            pageLength: 10,
+            dom: "<'row' <'col-md-12'B>><'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r><'table-scrollable't><'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>",
+        });
+    });
+</script>
 @endpush
