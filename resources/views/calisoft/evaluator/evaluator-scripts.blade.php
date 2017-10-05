eab30abb0624b59a2c8556c901238728523aae21
jhfredy <jhfredy84@hotmail.com>
2017-10-04 20:45:20 -0500

    arreglo de la interfaz calificar scripts

--- resources/views/calisoft/evaluator/evaluator-scripts.blade.php
+++ resources/views/calisoft/evaluator/evaluator-scripts.blade.php
@@ -15,11 +15,28 @@
                             <i class="fa fa-arrow-circle-right"></i>
                             Calificación Automatica
                         </button>
+                        
                         <br>
                         <tabla-items :items="items"></tabla-items>
+                        <div class="col-md-6">
+                        <h4>Calificación Total: <span class="label label-success">0</span></h3>
+                        </div>
+                     <div class ="row">   
+                        <div class="col-md-6">
+                            <button type="button"  class="btn blue center-block">
+                            <i class="fa fa-edit"></i>
+                            Editar Calificación
+                            </button>
+                            <br>
+                            <button type="button"  class="btn green-jungle center-block">
+                            <i class="fa fa-edit"></i>
+                            Aceptar Calificación
+                            </button>
+                        </div>
+                        </div>
+                        <br>
                         <textarea-input name="observacion" label="Observación" v-model="observacion"/>
                             
-                        
                     </div>
                 </div>
             </div>
