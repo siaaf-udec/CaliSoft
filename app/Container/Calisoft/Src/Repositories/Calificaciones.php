4f21e4a88aafd263cbf694bb4dee8a8f381ca0a8
jhfredy <jhfredy84@hotmail.com>
2018-02-26 20:39:15 -0500

    parte 1 calificacion final codificacion

--- app/Container/Calisoft/Src/Repositories/Calificaciones.php
+++ app/Container/Calisoft/Src/Repositories/Calificaciones.php
@@ -3,6 +3,9 @@
 namespace App\Container\Calisoft\Src\Repositories;
 
 use App\Container\Calisoft\Src\Proyecto;
+use App\Container\Calisoft\Src\ItemsCodificacion;
+use Illuminate\Support\Collection;
+
 
 /**
  * Calificaciones generales de los modulos
@@ -47,11 +50,33 @@ class Calificaciones
 
     public function codificacion()
     {
+        $promedios=Collect();
+        $items=ItemsCodificacion::all()->pluck('item','valor','PK_Id');
+        foreach($items as $item){
+            $query=$this->query($item);
+            $promedios->push(['promedio'=>$query->avg('nota') * $item->valor,
+            'prioridad'=>$item->valor
+            ]);
+        }
 
     }
 
     public function total()
     {
 
+    }
+    
+    public function query($item){
+        $query=DB::table('TBL_Proyectos')
+        ->join('TBL_Scripts','TBL_Scripts.FK_ProyectoId','=','TBL_Proyectos.PK_Id')
+        ->join('TBL_NotaCodificacion','TBL_NotaCodificacion.FK_ScriptsId','=','TBL_Scripts.PK_Id')
+        ->join('TBL_ItemsCodificacion','TBL_NotaCodificacion.FK_ItemsId','=','TBL_ItemsCodificacion.PK_Id')
+        ->select('TBL_Proyectos.PK_Id','TBL_NotaCodificacion.nota','TBL_ItemsCodificacion.item','TBL_ItemsCodificacion.valor','TBL_NotaCodificacion.total')
+        ->where('TBL_NotaCodificacion.total','<>','0')
+        ->where('TBL_Proyectos.PK_id','=', $proyecto->PK_id)
+        ->where('TBL_NotaCodificacion.FK_ItemsId','=',$item->PK_id)
+        ->get();
+
+        return $query;
     }
 }
