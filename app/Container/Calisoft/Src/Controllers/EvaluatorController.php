6c42d0e6cb9ba6a35170f1d9c3ce51b6253b4cca
Unknown <efrainvergara.udec@gmail.com>
2018-03-28 02:04:21 -0500

    Falta organizar en tablas la informacion del modal

--- app/Container/Calisoft/Src/Controllers/EvaluatorController.php
+++ app/Container/Calisoft/Src/Controllers/EvaluatorController.php
@@ -46,35 +46,24 @@ class EvaluatorController extends Controller
         return view('calisoft.evaluator.evaluator-scripts',compact('script'));
     }
     public function basedatos(Proyecto $proyecto){
-        return view('calisoft.evaluator.evaluator-basedatos',compact('proyecto'));
-    }
 
-    public function escenario(Proyecto $proyecto, CasoPrueba $casoPrueba)
-    {
-        return view('calisoft.evaluator.evaluator-escenario', compact('proyecto','casoPrueba'));
-    }
-
-    public function analizesql(Proyecto $proyecto, CalificacionBDRequest $request, CalificacionBD $calificacionbd)
-    {
         $nomenclaturabd = NomenclaturaBd::
                     select('tbl_tiponomenclatura.nomenclatura')
                           ->get();
-                    
+
         $palabra_info = "";
         $palabra_infos = "";
         $palabra = "";
         $palabras = "";
         $totalImpostantesBD ="";
         $totalEstandarBD ="";
-        $mensajeEncontradas = "Palabras Encontradas: ";
-        $mensajePropias = "Palabras Encontradas Propias del SQL: ";
-        $mensajeLineas = "Las palabras estan en la linea: ";
-        $importantesBD = array('CREATE TABLE', 'PRIMARY KEY', 'FOREIGN KEY', 'klkkñlk', 'jknkjnkjn', 'jnojhojuoi','okjlkjl','kjbkjnkj','kjnkjnj');
+  
+        $importantesBD = array('BASE DATOS','CREATE SCHEMA', 'CREATE TABLE', 'VIEWS', 'PRIMARY KEY', 'FOREIGN KEY', 'klkkñlk', 'jknkjnkjn', 'jnojhojuoi');
         $estandarBD = array();
 
         foreach($nomenclaturabd as $nomenbds)
         {
-        $estandarBD[] = $nomenbds->nomenclatura;
+            $estandarBD[] = $nomenbds->nomenclatura;
         }
 
         $rutaArchivo = storage_path() . "/app/uploads/sql/".$proyecto->sql->url;
@@ -92,55 +81,17 @@ class EvaluatorController extends Controller
             $palabra_infos .= "$repeticion,";
             $array = explode(",", $palabra_infos);
             $palabras .= "$i ($repeticion)<br>";
-    
-            if(strpos($leerArchivo, $i)> -1)
-            {
-            $mensajePropias .= $i . ', ';
-            }
+
 
         }
 
-        echo rtrim($mensajePropias, ", ");
-
-        echo "<br>",$palabra_infos,"<br>"; 
-
-        echo "Total Palabras Propias del SQL: ".$totalImpostantesBD, "<br>";
-
         foreach ($estandarBD as $k) 
         {
-
             $repeticiones = substr_count($leerArchivo, $k); 
             $totalEstandarBD += substr_count($leerArchivo, $k);
             $palabra_info .= "$repeticiones,"; 
             $array1 = explode(",", $palabra_info);
             $palabra .= "$k ($repeticiones)<br>"; 
-
-            if(strpos($leerArchivo, $k)> -1)
-            {
-                $mensajeEncontradas .= $k . ', ';
-            }
-        }
-
-        echo rtrim("<br>".$mensajeEncontradas, ", ");  
-
-        echo "<br>",$palabra; 
-
-        
-
-        echo "<br>Total Palabras Estandar Encontradas: ".$totalEstandarBD, "<br>";
-
-        $pos = 1;
-
-        foreach($rutalecturaArchivo as $linea)
-        {
-            for ( $x = 0; $x < count ( $importantesBD ); $x++ )
-            {
-                if (strstr($linea,$importantesBD[$x]))
-                {
-                            echo "<br> si esta la palabra $linea, está en la linea : ".$pos." <br>";
-                }
-                    $pos++;
-            }
         }
 
         // array total en la calificacion
@@ -193,24 +144,15 @@ class EvaluatorController extends Controller
             $sql = "UPDATE tbl_calificacionbd SET total= ?, acertadas= ?   WHERE FK_TipoNomenclaturaId=? and FK_ArchivoBdId= ?";
             DB::update($sql, array($total8, $acertadas8,9,  $proyecto->PK_id));
 
-            echo $proyecto->sql->url;
 
-          return view('calisoft.evaluator.evaluator-analizesql',compact('proyecto'),[
-            'nomenbd' => $nomenclaturabd,
-          ])->with('mensajeEncontradas', $mensajeEncontradas)
-            ->with('mensajePropias', $mensajePropias)->with('mensajeLineas', $mensajeLineas)
-            ->with('palabra_info', $palabra_info)->with('palabra_infos', $palabra_infos)
-            ->with('totalImpostantesBD', $totalImpostantesBD)->with('totalEstandarBD', $totalEstandarBD)
-            ->with('importantesBD', $importantesBD)->with('estandarBD', $estandarBD)
-            ->with('rutaArchivo', $rutaArchivo)->with('rutalecturaArchivo', $rutalecturaArchivo)
-            ->with('abrirArchivo', $abrirArchivo)->with('obtenerArchivo', $obtenerArchivo)
-            ->with('leerArchivo', $leerArchivo);
+        return view('calisoft.evaluator.evaluator-basedatos',compact('proyecto'),[
+            'nomenclaturabd' => $nomenclaturabd,
+          ]);
+    }
 
-         
-
-
+    public function escenario(Proyecto $proyecto, CasoPrueba $casoPrueba)
+    {
+        return view('calisoft.evaluator.evaluator-escenario', compact('proyecto','casoPrueba'));
     }
     
-    
-
 }
--- resources/views/calisoft/evaluator/evaluator-basedatos.blade.php
+++ resources/views/calisoft/evaluator/evaluator-basedatos.blade.php
@@ -13,10 +13,115 @@
                         <code-preview url="{{$proyecto->sql->url}}" prefix="/api/sql/preview/" mode="text/x-sql"></code-preview>                  
                     </div>
                     <div class="col-md-6">
-                           <a href="/proyectos/{{$proyecto->PK_id}}/analizesql"><button type="button"  class="btn green-jungle center-block">
+                        <!-- Trigger the modal with a button -->
+                        <button type="button" class="btn green-jungle center-block" data-toggle="modal" data-target="#ModalAutomatica">Calificación Automática <i class="fa fa-arrow-circle-right"></i></button>
+
+                            <!-- Modal -->
+                            <div id="ModalAutomatica" class="modal fade" data-backdrop="static" data-keyboard="false" role="dialog">
+                            <div class="modal-dialog modal-lg" style="overflow-y: scroll; max-height:85%;  margin-top: 50px; margin-bottom:50px;">
+
+                                <!-- Modal content-->
+                                <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    <h4 class="modal-title" align="center">Calificación Nomenclatura Base de datos - Proyecto: "{{$proyecto->nombre}}" </h4>
+                                </div>
+                                <div class="modal-body" >
+                                <?php
+                                $palabra_info = "";
+                                $palabra_infos = "";
+                                $palabra = "";
+                                $palabras = "";
+                                $totalImpostantesBD ="";
+                                $totalEstandarBD ="";
+                                $mensajeEncontradas = "Palabras Encontradas: ";
+                                $mensajePropias = "Palabras Encontradas Propias del SQL: ";
+                                $mensajeLineas = "Las palabras estan en la linea: ";
+                                $importantesBD = array('BASE DATOS','CREATE SCHEMA', 'CREATE TABLE', 'VIEWS', 'PRIMARY KEY', 'FOREIGN KEY', 'klkkñlk', 'jknkjnkjn', 'jnojhojuoi');
+                                $estandarBD = array();
+
+                                foreach($nomenclaturabd as $nomenbds)
+                                {
+                                    $estandarBD[] = $nomenbds->nomenclatura;
+                                }
+
+                                $rutaArchivo = storage_path() . "/app/uploads/sql/".$proyecto->sql->url;
+                                $rutalecturaArchivo = file($rutaArchivo);
+                                $abrirArchivo=fopen($rutaArchivo, "r+");
+
+                                $obtenerArchivo = fgets($abrirArchivo);
+                                $leerArchivo = fread($abrirArchivo, 350000);
+
+                                foreach ($importantesBD as $i) 
+                                {
+
+                                    $repeticion = substr_count($leerArchivo, $i); 
+                                    $totalImpostantesBD += substr_count($leerArchivo, $i); 
+                                    $palabra_infos .= "$repeticion,";
+                                    $array = explode(",", $palabra_infos);
+                                    $palabras .= "$i ($repeticion)<br>";
+                            
+                                    if(strpos($leerArchivo, $i)> -1)
+                                    {
+                                    $mensajePropias .= $i . ', ';
+                                    }
+
+                                }
+
+                                echo rtrim($mensajePropias, ", ");
+
+                                echo "<br>",$palabras,"<br>"; 
+
+                                echo "Total Palabras Propias del SQL: ".$totalImpostantesBD, "<br>";
+
+                                foreach ($estandarBD as $k) 
+                                {
+
+                                    $repeticiones = substr_count($leerArchivo, $k); 
+                                    $totalEstandarBD += substr_count($leerArchivo, $k);
+                                    $palabra_info .= "$repeticiones,"; 
+                                    $array1 = explode(",", $palabra_info);
+                                    $palabra .= "$k ($repeticiones)<br>"; 
+
+                                    if(strpos($leerArchivo, $k)> -1)
+                                    {
+                                        $mensajeEncontradas .= $k . ', ';
+                                    }
+                                }
+
+                                echo rtrim("<br>".$mensajeEncontradas, ", ");  
+
+                                echo "<br>",$palabra; 
+
+                                
+
+                                echo "<br>Total Palabras Estandar Encontradas: ".$totalEstandarBD, "<br>";
+
+                                /*$pos = 1;
+
+                                foreach($rutalecturaArchivo as $linea)
+                                {
+                                    for ( $x = 0; $x < count ( $importantesBD ); $x++ )
+                                    {
+                                        if (strstr($linea,$importantesBD[$x]))
+                                        {
+                                                    echo "<br> si esta la palabra $linea, está en la linea : ".$pos." <br>";
+                                        }
+                                            $pos++;
+                                    }
+                                }*/
+                                ?>
+                                </div>
+                                <div class="modal-footer">
+                                    <a href="/proyectos/{{$proyecto->PK_id}}/basedatos"><button type="button"  class="btn green-jungle center-block">
                                     <i class="fa fa-arrow-circle-right"></i>
                                     Calificación Automatica
                                 </button></a> 
+                                </div>
+                                </div>
+
+                            </div>
+                            </div>
                         <h5><center><strong>Información</strong></center></h5>
                         <tabla-componente :read="false"></tabla-componente>  
                         <form method="POST" action="{{route('observacion-bd',['sql'=>$proyecto->sql->PK_id])}}">
